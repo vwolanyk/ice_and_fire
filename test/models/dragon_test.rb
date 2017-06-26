4c6be27a9f574617a882810039488b85f07ca149
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-26 13:21:34 -0400

    Test passed Dragon age and period alive

--- test/models/dragon_test.rb
+++ test/models/dragon_test.rb
@@ -19,7 +19,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_young_dragons_from_asoiaf
     # use your knowledge of factories to make this test pass!
-    young_dragon = nil
+    young_dragon = build(:dragon, age:2)
 
     # not allowed to change the assert statement!
     skip unless young_dragon
@@ -28,16 +28,16 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_middle_dragons_from_targ_dynasty
     # use your knowledge of factories to make this test pass!
-    middle_dragon = nil
+    middle_dragon = build(:dragon, age: 11)
 
     # not allowed to change the assert statement!
     skip unless middle_dragon
-    assert_equal( 'Targaryen Dynasty', middle_dragon.period_alive)
+    assert_equal( 'The Fall of the Dragons', middle_dragon.period_alive)
   end
 
   def test_that_ancient_dragons_from_age_of_valyria
     # use your knowledge of factories to make this test pass!
-    ancient_dragon = nil
+    ancient_dragon = build(:dragon, age: 999)
 
     # not allowed to change the assert statement!
     skip unless ancient_dragon
